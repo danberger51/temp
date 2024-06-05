9220e1cf64a4b3bc76635f5213e4d5c9005a62de
DAN BERGER <dan.berger@ict-campus.net>
2024-06-05 13:43:48 +0200

    yur

--- src/functions/TestLabor.js
+++ src/functions/TestLabor.js
@@ -1,8 +1,8 @@
 const { app,input } = require('@azure/functions');
 
 const cosmosInput = input.cosmosDB({
-    databaseName: 'DemoDatabase',
-    containerName: 'Items',
+    databaseName: 'freenemi',
+    containerName: 'yurr',
     connection: 'CosmosDB',
     sqlQuery: "select * from c"
 });
