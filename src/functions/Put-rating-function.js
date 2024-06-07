e04a5e8e096cab5b6462ade94ea78994a0dd2b63
Julia Ebner <julia.ebner@ict-campus.net>
2024-06-07 09:47:59 +0200

    First try put

---
+++ src/functions/Put-rating-function.js
@@ -0,0 +1,41 @@
+const { app, input, output } = require('@azure/functions');
+
+const cosmosInput = input.cosmosDB({
+    databaseName: 'FilmDatabase',
+    containerName: 'Films',
+    connection: 'CosmosDB',
+    sqlQuery: "SELECT * FROM c WHERE c.id = @filmId",
+    parameters: [
+        {
+            name: "@filmId",
+            value: ""
+        }
+    ]
+});
+
+const cosmosOutput = output.cosmosDB({
+    databaseName: 'FilmDatabase',
+    containerName: 'Films',
+    connection: 'CosmosDB',
+    createIfNotExists: true,
+});
+
+app.http('putItems', {
+    methods: ['PUT'],
+    authLevel: 'anonymous',
+    extraInputs: [cosmosInput],
+    extraOutputs: [cosmosOutput],
+    route: 'items/{id}',
+    handler: async (request, context) => {
+        const items = context.extraInputs.get(cosmosInput);
+        const data = await request.json();
+        data.id = item[0].id;
+
+        context.extraOutputs.set(cosmosOutput, data)
+
+        return {
+            body: JSON.stringify(data),
+            status: 200
+        };
+    }
+});
